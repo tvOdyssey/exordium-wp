4e578c2adef9c9407c3b882a37150e75580bdfec
Odyssey <admin@odyssey-vpn.com>
2019-12-30 07:56:42 -0500

    Add featured image support, and fix the breadcrumb offset.

--- page.php
+++ page.php
@@ -12,14 +12,14 @@ if ( is_front_page() ) {
 
 # Page Content
 if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-      if ( is_front_page() ) {
-        the_content();
-      } else {
-        # Block for featured image
-        if ( has_post_thumbnail() ) {
+  while ( have_posts() ) : the_post();
+    if ( is_front_page() ) {
+      the_content();
+    } else {
+      # Block for featured image
+      if ( has_post_thumbnail() ) {
 ?>
-<section class="bg-dark text-light header-inner p-0 jarallax o-hidden" data-overlay data-jarallax data-speed="0.2">
+<section class="bg-dark text-light header-inner p-0 jarallax o-hidden" data-overlay data-jarallax data-speed="0.2" style="padding-top: 73px !important;">
   <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Image" class="jarallax-img opacity-30">
   <div class="container py-0 layer-2">
     <div class="row my-3">
@@ -29,32 +29,29 @@ if ( have_posts() ) :
             <li class="breadcrumb-item">
               <a href="index.html">Home</a>
             </li>
-            <li class="breadcrumb-item">
-              <a href="#">Pages</a>
-            </li>
-            <li class="breadcrumb-item active" aria-current="page">Current Page</li>
+            <li class="breadcrumb-item active" aria-current="page"><?php the_title(); ?></li>
           </ol>
         </nav>
       </div>
     </div>
     <div class="row my-4 my-md-6" data-aos="fade-up">
       <div class="col-lg-9 col-xl-8">
-        <h1 class="display-4">About Us</h1>
+        <h1 class="display-4"><?php the_title(); ?></h1>
         <p class="lead mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
       </div>
     </div>
   </div>
   <div class="decoration-wrapper">
     <div class="decoration bottom right d-none d-md-block" data-jarallax-element="100 100">
-      <img class="bg-primary-2" src="assets/img/decorations/deco-blob-1.svg" alt="deco-blob-1 decoration" data-inject-svg />
+      <img class="bg-primary-2" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/decorations/deco-blob-1.svg" alt="deco-blob-1 decoration" data-inject-svg />
     </div>
   </div>
   <div class="divider flip-x">
-    <img src="assets/img/dividers/divider-1.svg" alt="graphical divider" data-inject-svg />
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/dividers/divider-1.svg" alt="graphical divider" data-inject-svg />
   </div>
 </section>
 <?php
-        }
+      }
 ?>
 <section>
   <div class="container">
