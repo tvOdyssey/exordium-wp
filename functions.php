016dda9c5d3e6534cff812493e7f181143d26a55
Odyssey <admin@odyssey-vpn.com>
2019-12-29 21:29:16 -0500

    Add wordpress theme support for 3 custom menus.

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 
-# Exordium Wordpress Theme Functions
+/**
+ * Exordium Wordpress Functions Page
+ * Tie wordpress to this theme
+ */
+
+# Add support for the wordpress html <title> tag.
 if ( !function_exists( 'exordium_setup' ) ) {
     function exordium_setup() {
         add_theme_support( 'title-tag' );
@@ -8,5 +13,17 @@ if ( !function_exists( 'exordium_setup' ) ) {
 
     add_action( 'after_setup_theme', 'exordium_setup' );
 }
+
+# Add support for the 3 main menus.
+function exordium_new_menus() {
+  register_nav_menus(
+    array(
+      'primary-menu' => __( 'Primary Menu' ),
+      'hamburger-menu' => __( 'Hamburger Menu' ),
+      'footer-menu' => __( 'Footer Menu' )
+    )
+  );
+}
+add_action( 'init', 'exordium_new_menus' );
 
 ?>
