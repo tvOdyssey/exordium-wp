dde30c24ab2fc084e2d253ed98a8e90b98a33435
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2019-12-30 23:15:32 -0500

    Move javascript to the functions.php, load with wordpress

--- footer.php
+++ footer.php
@@ -75,19 +75,7 @@
       </div>
     </footer>
 
-
-    <!-- required vendors -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-    <!-- plugin vendors -->
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/fancyapps/jquery.fancybox.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/jarallax/jarallax.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/jarallax/jarallax-video.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/plugins/jarallax/jarallax-element.min.js"></script>
-
-    <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
   </body>
 </html>
