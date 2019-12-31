df41b4d0b589c2d5d9777aa19687b063d6c2a464
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2019-12-30 22:24:36 -0500

    Upgrade jQuery version

--- functions.php
+++ functions.php
@@ -39,6 +39,15 @@ function my_wp_subtitle_page_part_support() {
 }
 add_action( 'init', 'my_wp_subtitle_page_part_support' );
 
+# Upgrade jQuery and Migrate
+function replace_core_jquery_version() {
+    wp_deregister_script( 'jquery-core' );
+    wp_register_script( 'jquery-core', "https://code.jquery.com/jquery-3.1.1.min.js", array(), '3.1.1' );
+    wp_deregister_script( 'jquery-migrate' );
+    wp_register_script( 'jquery-migrate', "https://code.jquery.com/jquery-migrate-3.0.0.min.js", array(), '3.0.0' );
+}
+add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
+
 # Include Javascript Files
 function exordium_register_js() {
   wp_enqueue_script( 'fontawesome', get_template_directory_uri() . '/assets/plugins/fontawesome/all.js', array ( 'jquery' ), 1.0, true);
