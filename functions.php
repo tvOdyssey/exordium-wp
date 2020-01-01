ec7295d2e886f2b9cb182fd25ce095cd7dd91397
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-01 01:00:56 -0500

    Adding theme support now activates most plguins to work

--- functions.php
+++ functions.php
@@ -89,6 +89,9 @@ function exordium_register_js() {
   wp_enqueue_script( 'jarallax-video', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax-video.min.js', array ( 'jquery' ), 1.0, true);
   wp_enqueue_script( 'jarallax-element', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax-element.min.js', array ( 'jquery' ), 1.0, true);
 
+  # Main Functions Include
+  wp_enqueue_script( 'exordium', get_template_directory_uri() . '/assets/js/exordium.js', array ( 'jquery' ), 1.0, true);
+
 }
 add_action( 'init', 'exordium_register_js' );
 
