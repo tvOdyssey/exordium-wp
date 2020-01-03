40241c280e576dbd85edeea9997bb8bd8128b125
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-03 09:21:35 -0500

    Custom about-nav that loads required Categories and posts finished.

--- partials/about-nav.php
+++ partials/about-nav.php
@@ -1,37 +1,25 @@
 <div class="container py-4" data-dropdown-content>
   <div class="row">
-    <div class="col-lg col-md-6 mb-3 mb-lg-0">
-      <h5>General</h5>
-      <div>
-        <a class="dropdown-item fade-page" href="#">Locations</a>
-        <a class="dropdown-item fade-page" href="#">Network Status</a>
-      </div>
-    </div>
-    <div class="col-lg col-md-6 mb-3 mb-lg-0">
-      <h5>Resources</h5>
-      <div>
-        <a class="dropdown-item fade-page" href="#">Reasons for a VPN</a>
-        <a class="dropdown-item fade-page" href="#">Tutorials</a>
-        <a class="dropdown-item fade-page" href="#">Documentation</a>
-      </div>
-    </div>
-    <div class="col-lg col-md-6 mb-3 mb-lg-0">
-      <h5>Blog</h5>
-      <div>
-        <a class="dropdown-item fade-page" href="#">Why Odyssey?</a>
-        <a class="dropdown-item fade-page" href="#">Browsing Anonymously</a>
-        <a class="dropdown-item fade-page" href="#">Public Security</a>
-        <a class="dropdown-item fade-page" href="#">Data Theft</a>
-        <a class="dropdown-item fade-page" href="#">Hiding Your Real Location</a>
-      </div>
-    </div>
-    <div class="col-lg col-md-6 mb-3 mb-lg-0">
-      <h5>Company</h5>
-      <div><a class="dropdown-item fade-page" href="#">About Us</a>
-        <a class="dropdown-item fade-page" href="#">Our Team</a>
-        <a class="dropdown-item fade-page" href="#">Service Reviews</a>
-        <a class="dropdown-item fade-page" href="#">Jobs at Odyssey</a>
-      </div>
-    </div>
+
+    <?php
+      $allowedCats = array("General", "Resources", "Blog", "Company");
+      $categories = get_categories();
+      foreach  ($categories as $category) {
+        if ( in_array($category->name, $allowedCats) ) {
+          //Display the sub category information using $category values like $category->cat_name
+          echo '<div class="col-lg col-md-6 mb-3 mb-lg-0">';
+            echo '<h5>'.$category->name.'</h5>';
+            echo '<div>';
+              foreach (get_posts('cat='.$category->term_id) as $post) {
+                setup_postdata( $post );
+                echo '<a class="dropdown-item fade-page" href="'.get_permalink($post->ID).'">'.get_the_title().'</a>';
+              }
+            echo '</div>';
+          echo '</div>';
+        }
+      }
+      wp_reset_query();
+    ?>
+
   </div>
 </div>
