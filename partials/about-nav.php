8d7a07d8d4fdc5934e20f3d2ab2f417fea46781e
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-04 03:19:03 -0500

    Make sure we are printing the posts the way we want.

--- partials/about-nav.php
+++ partials/about-nav.php
@@ -6,24 +6,31 @@
       $allowedCats = array("General", "Resources", "Blog", "Company");
 
       # Display the categories the way we want
-      $args = array(
+      $catArgs = array(
         'orderby' => 'include',
         'order' => 'ASC',
         'include' => '8,9,10,11'
       );
 
-      # Print the navigation out. 
-      $categories = get_categories( $args );
+      # Print the navigation out.
+      $categories = get_categories( $catArgs );
       foreach  ($categories as $category) {
         if ( in_array($category->name, $allowedCats) ) {
+
+          $postArgs = array(
+            'category' => $category->term_id,
+            'orderby' => 'date',
+            'order' => 'ASC'
+          );
+
           //Display the sub category information using $category values like $category->cat_name
           echo '<div class="col-lg col-md-6 mb-3 mb-lg-0">';
             echo '<h5>'.$category->name.'</h5>';
             echo '<div>';
-              foreach (get_posts('cat='.$category->term_id) as $post) {
-                setup_postdata( $post );
-                echo '<a class="dropdown-item fade-page" href="'.get_permalink($post->ID).'">'.get_the_title().'</a>';
-              }
+            foreach (get_posts($postArgs) as $post) {
+              setup_postdata( $post );
+              echo '<a class="dropdown-item fade-page" href="'.get_permalink($post->ID).'">'.get_the_title().'</a>';
+            }
             echo '</div>';
           echo '</div>';
         }
