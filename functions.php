ab2869f952e74f6e4d7353f5f8bd26e41b3113ee
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-07 15:19:05 -0500

    Uhh errors wtf

--- functions.php
+++ functions.php
@@ -80,40 +80,38 @@ add_action( 'init', 'exordium_register_css' );
 
 # Include Javascript Files
 function exordium_register_js() {
-  if ( ! is_admin() && ! is_login_page() ) {
-    # Bootstrap Scripts
-    wp_enqueue_script( 'popper.min', get_template_directory_uri() . '/assets/js/popper.min.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'boostrap.min', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array ( 'jquery' ), 1.0, true);
+  # Bootstrap Scripts
+  wp_enqueue_script( 'popper.min', get_template_directory_uri() . '/assets/js/popper.min.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'boostrap.min', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array ( 'jquery' ), 1.0, true);
 
-    # FontAwesome Include
-    wp_enqueue_script( 'fontawesome', get_template_directory_uri() . '/assets/plugins/fontawesome/all.js', array ( 'jquery' ), 2.0, true);
+  # FontAwesome Include
+  wp_enqueue_script( 'fontawesome', get_template_directory_uri() . '/assets/plugins/fontawesome/all.js', array ( 'jquery' ), 2.0, true);
 
-    # Theme Related Plugins
-    wp_enqueue_script( 'aos', get_template_directory_uri() . '/assets/js/aos.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'scrollMonitor', get_template_directory_uri() . '/assets/js/scrollMonitor.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'svg-injector', get_template_directory_uri() . '/assets/js/svg-injector.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'countdown', get_template_directory_uri() . '/assets/js/jquery.countdown.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'flatpicker', get_template_directory_uri() . '/assets/js/flatpicker.js', array ( 'jquery' ), 1.0,true);
-    wp_enqueue_script( 'flickity', get_template_directory_uri() . '/assets/js/flickity.js', array ( 'jquery' ), 1.0,  true);
-    wp_enqueue_script( 'rangeSlider', get_template_directory_uri() . '/assets/js/ion.rangeSlider.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'isotope', get_template_directory_uri() . '/assets/js/isotope.pkgd.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'polyr', get_template_directory_uri() . '/assets/js/plyr.polyfilled.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'prism', get_template_directory_uri() . '/assets/js/prism.js', array ( 'jquery' ), 1.0,true);
-    wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/assets/js/smooth-scroll.polyfills.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'twitterFetcher', get_template_directory_uri() . '/assets/js/twitterFetcher.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'typed', get_template_directory_uri() . '/assets/js/typed.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'smartWizard', get_template_directory_uri() . '/assets/js/jquery.smartWizard.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'fancybox', get_template_directory_uri() . '/assets/plugins/fancyapps/jquery.fancybox.min.js', array ( 'jquery' ), 1.0, true);
+  # Theme Related Plugins
+  wp_enqueue_script( 'aos', get_template_directory_uri() . '/assets/js/aos.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'scrollMonitor', get_template_directory_uri() . '/assets/js/scrollMonitor.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'svg-injector', get_template_directory_uri() . '/assets/js/svg-injector.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'countdown', get_template_directory_uri() . '/assets/js/jquery.countdown.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'flatpicker', get_template_directory_uri() . '/assets/js/flatpicker.js', array ( 'jquery' ), 1.0,true);
+  wp_enqueue_script( 'flickity', get_template_directory_uri() . '/assets/js/flickity.js', array ( 'jquery' ), 1.0,  true);
+  wp_enqueue_script( 'rangeSlider', get_template_directory_uri() . '/assets/js/ion.rangeSlider.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'isotope', get_template_directory_uri() . '/assets/js/isotope.pkgd.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'polyr', get_template_directory_uri() . '/assets/js/plyr.polyfilled.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'prism', get_template_directory_uri() . '/assets/js/prism.js', array ( 'jquery' ), 1.0,true);
+  wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/assets/js/smooth-scroll.polyfills.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'twitterFetcher', get_template_directory_uri() . '/assets/js/twitterFetcher.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'typed', get_template_directory_uri() . '/assets/js/typed.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'smartWizard', get_template_directory_uri() . '/assets/js/jquery.smartWizard.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'fancybox', get_template_directory_uri() . '/assets/plugins/fancyapps/jquery.fancybox.min.js', array ( 'jquery' ), 1.0, true);
 
-    # Jarallax Scripts
-    wp_enqueue_script( 'jarallax.min', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax.min.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'jarallax-video', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax-video.min.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'jarallax-element', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax-element.min.js', array ( 'jquery' ), 1.0, true);
+  # Jarallax Scripts
+  wp_enqueue_script( 'jarallax.min', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax.min.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'jarallax-video', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax-video.min.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'jarallax-element', get_template_directory_uri() . '/assets/plugins/jarallax/jarallax-element.min.js', array ( 'jquery' ), 1.0, true);
 
-    # Main Functions Include
-    wp_enqueue_script( 'exordium', get_template_directory_uri() . '/assets/js/exordium.js', array ( 'jquery' ), 1.0, true);
-    wp_enqueue_script( 'custom-exordium', get_template_directory_uri() . '/assets/js/custom.js', array ( 'jquery' ), 1.0, true);
-  }
+  # Main Functions Include
+  wp_enqueue_script( 'exordium', get_template_directory_uri() . '/assets/js/exordium.js', array ( 'jquery' ), 1.0, true);
+  wp_enqueue_script( 'custom-exordium', get_template_directory_uri() . '/assets/js/custom.js', array ( 'jquery' ), 1.0, true);
 }
 add_action( 'init', 'exordium_register_js' );
 
