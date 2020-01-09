5c22e65df43fc9fc6e3cb226f4465e16b03f14a4
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-09 03:18:44 -0500

    Add in the beginning for the new layout and fallback pages for searches, and archives.

--- index.php
+++ index.php
@@ -3,6 +3,25 @@
 # Include the header file
 get_header();
 
+# Last Resort
+get_template_part('partials/nav/nav-page-light');
+
+if ( is_search() ) {
+  global $wp_query;
+
+  $archive_title = 'Searching for ' . get_search_query();
+  $archive_subtitle = 'There were a total of ' . $wp_query->found_posts . ' found';
+} elseif ( !is_home() ) {
+  $archive_title = get_the_archive_title();
+  $archive_subtitle = get_the_archive_description();
+}
+
+set_query_var( 'archive_title', $archive_title );
+set_query_var( 'archive_subtitle', $archive_subtitle );
+
+if ( $archive_title || $archive_subtitle ) {
+  get_template_part('partials/cover/cover-archive-transparent');
+}
 
 # What are we right now?
 if ( have_posts() ) :
