c982617905d827ec49241f27e18ce02cd2a6cebf
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-01-08 23:38:00 -0500

    Post design for only pages that have 1 single post on them, we should have a fallback for the category/topic/search pages on index.php

---
+++ single.php
@@ -0,0 +1,30 @@
+<?php
+
+# Include the header file
+get_header();
+
+
+# What are we right now?
+if ( have_posts() ) :
+  while ( have_posts() ) : the_post();
+
+    # Cover Section
+    if ( has_post_thumbnail() ) {
+      get_template_part('partials/nav/nav-page-transparent');
+      get_template_part('partials/cover/cover-post-transparent');
+    } else {
+      get_template_part('partials/nav/nav-page-light');
+      get_template_part('partials/cover/cover-light');
+    }
+
+    # Content Section
+    get_template_part('partials/content/content-post');
+  endwhile;
+else :
+  // When no posts are found, output this text.
+  _e( 'Sorry, no posts matched your criteria.' );
+endif;
+wp_reset_postdata();
+
+# Footer
+get_footer();
